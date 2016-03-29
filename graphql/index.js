db657465f465fd761a8a6fa1f9c3d2899f51c73c
Brian Capouch <brianc@palaver.net>
2016-03-29 15:23:43 -0400

    Add top-level descriptions

--- graphql/index.js
+++ graphql/index.js
@@ -9,10 +9,12 @@ import queries from './queries';
 export default new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'Query',
+    description: 'Query images and GeoPoint records',
     fields: queries
   }),
   mutation: new GraphQLObjectType({
     name: 'Mutation',
+    description: 'Add new image and GeoPoint records',
     fields: mutations
   })
 });
